665dc942874cbce7dd8d2ae4864589c947bc933f
Mark Williams <mwilliams@cj.com>
2021-12-05 11:53:21 -0800

    day 5 part 2

--- 05/main.mjs
+++ 05/main.mjs
@@ -4,30 +4,31 @@ const parsePoint = s => s.trim().split(",").map(Number);
 
 const parseSegment = s => s.split("->").map(parsePoint);
 
-const isHorizontal = segment => segment[0][1] === segment[1][1];
-const isVertical = segment => segment[0][0] === segment[1][0];
-const isLine = segment =>  isVertical(segment) ||  isHorizontal(segment);
+const isLineForPart1 = segment => {
+  const isHorizontal = segment => segment[0][1] === segment[1][1];
+  const isVertical = segment => segment[0][0] === segment[1][0];
+  return isVertical(segment) ||  isHorizontal(segment);
+};
 
 const toKey = a => a[0] + "," + a[1];
 const eq = (p1,p2) => p1[0] === p2[0] && p1[1] === p2[1];
 
-const minMax = segment => {
-  const [a,b] = segment;
-  return isHorizontal(segment) 
-    ? (a[0] < b[0] ? segment : [b,a])
-    : (a[1] < b[1] ? segment : [b,a]);
+const deltas = segment => {
+  const delta = n =>  n === 0 ? 0 : (n < 0 ? -1 : 1);
+  const diffX = segment[1][0] - segment[0][0];
+  const diffY = segment[1][1] - segment[0][1];
+  return [ delta(diffX), delta(diffY) ];
 };
 
 const allPointsInSegment = segment => {
   const last = a => a.slice(-1)[0];
-  const incX = p => [1 + p[0], p[1]];
-  const incY = p => [p[0], 1 + p[1]];
-  const [a, b] = minMax(segment);
-  const f = isHorizontal(segment) ? incX : incY;
+  const [dx, dy] = deltas(segment);
+  const nextPoint = p =>  [ p[0] + dx, p[1] + dy ];
+  const [a,b] = segment;
 
   function generate(points) {
     const prev = last(points);
-    return eq(b,prev) ? points : generate(points.concat([f(prev)]));
+    return eq(b,prev) ? points : generate(points.concat([nextPoint(prev)]));
   }
   return generate([a]);
 };
@@ -35,17 +36,20 @@ const allPointsInSegment = segment => {
 const countPoints = (counts, p) => Object.assign(counts, { [p]: 1 + (counts[p] || 0) });
 const countIntersections = (total, n) => n === 1 ? total : total + 1;
 
+const solve = (segments, segmentFilter) => {
+
+  // need intermediate value here because objects don't have a .values() method
+  const allPoints = segments.filter(segmentFilter)
+    .flatMap(allPointsInSegment)
+    .map(toKey)
+    .reduce(countPoints, {});
+
+  return Object.values(allPoints).filter(n => n > 1).length;
+}
+
 const input = fs.readFileSync("input.txt", "utf-8").split("\n").filter(s=>s);
 const segments = input.map(parseSegment);
-const lines = segments.filter(isLine);
 
-const allPoints = lines
-  .flatMap(allPointsInSegment)
-  .map(toKey)
-  .reduce(countPoints, {});
-
-// have to break the chain here because objects don't have a .values() method
-const solution = Object.values(allPoints).filter(n => n > 1).length;
-
-console.log(solution);
+console.log("part1", solve(segments, isLineForPart1));
+console.log("part2", solve(segments, () => true));
 
