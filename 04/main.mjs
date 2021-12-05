39fadc8a794d8030ebad6ef6feb9412703c8c388
Mark Williams <mwilliams@cj.com>
2021-12-05 08:58:41 -0800

    day 4 part 2

--- 04/main.mjs
+++ 04/main.mjs
@@ -1,11 +1,18 @@
 import * as fs from 'fs';
-const input = fs.readFileSync("input.txt", "utf-8").split("\n").filter(s=>s);
 
 const makeCell = s => ({value: s, marked: false});
-
 const makeRow = s => s.split(" ").filter(s=>s).map(Number).map(makeCell);
-
 const makeBoard = ss => ss.map(makeRow);
+
+const makeBoards = input => {
+  const makeBoards0 = (boards, n) => {
+    if (!input[n]) return boards;
+    const next = n + 5;
+    const board = makeBoard(input.slice(n, next));
+    return makeBoards0(boards.concat([board]), next);
+  };
+  return makeBoards0([], 1);
+}
 
 const row = (board, n) => board[n];
 const col = (board, n) => board.map(row=>row[n]);
@@ -21,33 +28,45 @@ const isWinner = board => {
   const hasWinningCol = [0,1,2,3,4].some(n=>allMarked(col(board,n)));
   return hasWinningRow || hasWinningCol;
 };
-const sum = (a,b) => a + b;
+
+const first = a => a[0];
+const last = a => first(a.slice(-1));
+const tail = a => a.slice(1);
 
 const score = board => {
-  const unmarkedCellValues = row => row.filter(cell=>!cell.marked).map(cell=>cell.value);
+  const sum = (a,b) => a + b;
+  const unmarkedCell = cell => !cell.marked;
+  const cellValue = cell => cell.value;
+  const unmarkedCellValues = row => row.filter(unmarkedCell).map(cellValue);
   return board.flatMap(unmarkedCellValues).reduce(sum);
 };
 
-const makeBoards = input => {
-  const makeBoards0 = (boards, n) => {
-    if (!input[n]) return boards;
-    const board = makeBoard(input.slice(n,n+5));
-    return makeBoards0(boards.concat([board]), n+5);
-  };
-  return makeBoards0([], 1);
-}
-
 const play = (numbers, boards) => {
-  const numberToPlay =  numbers[0];
-  boards.forEach(board => mark(board, numberToPlay));
-  const winningIndex = boards.findIndex(isWinner);
-  if (winningIndex >= 0) {
-    console.log("winner board",winningIndex);
-    console.log("score",numberToPlay * score(boards[winningIndex]));
-  }
-  else play(numbers.slice(1), boards);
+
+  const play0 = (numbers, boards, winningBoards, winningScores) => {
+    const hasAlreadyWon = index => winningBoards.includes(index);
+    const numberToPlay =  first(numbers);
+    boards.forEach((board, index) => {
+      if (!hasAlreadyWon(index)) {
+        mark(board, numberToPlay);
+        if (isWinner(board)) {
+          winningBoards.push(index);
+          winningScores.push(numberToPlay * score(board));
+        }
+      }
+    });
+    return (winningBoards.length === boards.length)
+      ? winningScores
+      : play0(tail(numbers), boards, winningBoards, winningScores);
+  };
+
+  return play0(numbers, boards, [], []);
 };
 
-const numbersToDraw = input[0].split(",").filter(s=>s).map(Number);
+const input = fs.readFileSync("input.txt", "utf-8").split("\n").filter(s=>s);
+const numbersToDraw = first(input).split(",").filter(s=>s).map(Number);
 const boards = makeBoards(input);
-play(numbersToDraw, boards);
+
+const winningScores = play(numbersToDraw, boards);
+console.log("part 1 - first winning board score", first(winningScores));
+console.log("part 2 - last winning board score", last(winningScores));
