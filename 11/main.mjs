38c8cd0a54b35081c645d0b11edb9ef92bc458b5
Mark Williams <mwilliams@cj.com>
2021-12-10 23:31:08 -0800

    day 11 part 1

---
+++ 11/main.mjs
@@ -0,0 +1,83 @@
+import * as fs from 'fs';
+const readFile = name =>fs.readFileSync(name, "utf-8").split("\n").filter(s=>s).map(s=>s.split(''));; 
+const sampleInput = readFile("sample.txt");
+const puzzleInput = readFile("input.txt");
+
+const getCell = (grid, {r,c}) => {
+  const row = grid[r];
+  return row ? row[c] : null;
+}
+
+const setCell = (grid, {r,c}, value) => grid[r][c] = value;
+
+const mapCells = (grid, f) => grid.map(row=>row.map(f));
+
+const render = grid => grid.map(row=>row.join("")).join("\n");
+
+const range = n => [...Array.from({length:n}).keys()];
+
+const makeLocations = (numRows, numCols) => {
+  const locations = [];
+  for (let r of range(numRows)) { 
+    for (let c of range(numRows)) { 
+      locations.push({r,c});
+    }
+  };
+  return locations;
+}
+
+const locations = makeLocations(10,10);
+
+const neigborLocations = (grid, rc) => {
+  const deltas = [
+    {r:0,c:1},
+    {r:0,c:-1},
+    {r:1,c:1},
+    {r:1,c:0},
+    {r:1,c:-1},
+    {r:-1,c:1},
+    {r:-1,c:0},
+    {r:-1,c:-1},
+  ];
+  return deltas.map(d=>({r:rc.r + d.r, c: rc.c + d.c}));
+}
+
+const inc = n => n + 1;
+
+const step = grid => {
+
+  const flash = (count, grid) => {
+    const flashLocation = locations.find(rc=>getCell(grid,rc) > 9);
+    //console.log("flashLocation", flashLocation);
+    if (!flashLocation) return {count,grid};
+    setCell(grid, flashLocation, 0);
+    neigborLocations(grid, flashLocation).forEach(rc=>{
+      const level = getCell(grid, rc);
+      //console.log("rc",rc,"level",level);
+      if (level && level >= 0) {
+        //console.log("setting level", rc, level +1);
+        setCell(grid, rc, level + 1);
+      }
+    });
+    return flash(count + 1, grid);;
+  };
+
+  return flash(0, mapCells(grid,inc));
+};
+
+const solvePart1 = input => {
+  const grid = mapCells(input, Number);
+  const result = range(100).reduce((acc, n)=> {
+    //console.log("n", n, "count", acc.count);
+    const next = step(acc.grid);
+    return { count: acc.count + next.count, grid: next.grid };
+  }, {count: 0, grid});
+
+  //console.log("flashes", result.count);
+  // console.log(render(result.grid));
+  return result.count;
+
+
+};
+
+console.log("part 1=", solvePart1(puzzleInput));
